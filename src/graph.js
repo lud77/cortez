65724ae8f0a0b671ba1cb3042612ba036dfb17ba
Stefano Butera <Stefano Butera stefano.butera@nethnic.com>
2017-05-13 17:14:24 +0100

    added tests for graph

--- src/graph.js
+++ src/graph.js
@@ -1,200 +1,197 @@
-import sequenceFactory from 'sequence';
+import sequenceFactory from './sequence';
 
-export default () => {
+const getId = (element) => typeof(element) === 'object' ? element.id : element;
 
-	const getId = (element) => typeof(element) === 'object' ? element.id : element;
+const fragment = (fragment, options = {}) => {
+	let { nodes, edges, nodeCount, edgeCount } = (typeof fragment === 'string') ? JSON.parse(fragment) : fragment;
 
-	const fragment = (fragment, options = {}) => {
-		const { nodes, edges, nodeCount, edgeCount } = (typeof fragment === 'string') ? JSON.parse(fragment) : fragment;
+	const nodeSeq = sequenceFactory(nodeCount);
+	const edgeSeq = sequenceFactory(edgeCount);
 
-		const nodeSeq = sequenceFactory(nodeCount);
-		const edgeSeq = sequenceFactory(edgeCount);
+	const pack = () => {
+		const newGraph = empty();
+		const mapping = {};
 
-		const pack = () => {
-			const newGraph = empty();
-			const mapping = {};
-
-			for (let n in this.nodes) {
-				if (this.nodes[n]) {
-					mapping[n] = newGraph.addNode(Object.assign({}, this.nodes[n]));
-				}
+		for (let n in nodes) {
+			if (nodes[n]) {
+				mapping[n] = newGraph.addNode(Object.assign({}, nodes[n]));
 			}
+		}
 
-			for (let e in this.edges) {
-				if (this.edges[e]) {
-					const id = newGraph.addAdge(Object.assign({}, this.edges[e]));
-					newGraph.edges[id].from = mapping[newGraph.edges[id].from];
-					newGraph.edges[id].to = mapping[newGraph.edges[id].to];
-				}
+		for (let e in edges) {
+			if (edges[e]) {
+				const id = newGraph.addAdge(Object.assign({}, edges[e]));
+				newGraph.edges[id].from = mapping[newGraph.edges[id].from];
+				newGraph.edges[id].to = mapping[newGraph.edges[id].to];
 			}
+		}
 
-			return newGraph;
-		};
-
-		const mergeWith = (fragment) => {
-			const operand = Graph.fragment(fragment);
-			const newGraph = this.pack();
-			const mapping = {};
-
-			for (let n in operand.nodes) {
-				if (operand.nodes[n]) {
-					mapping[n] = newGraph.addNode(Object.assign({}, operand.nodes[n]));
-				}
-			}
-
-			for (let e in operand.edges) {
-				if (operand.edges[e]) {
-					let id = newGraph.addEdge(Object.assign({}, operand.edges[e]));
-					newGraph.edges[id].from = mapping[newGraph.edges[id].from];
-					newGraph.edges[id].to = mapping[newGraph.edges[id].to];
-				}
-			}
-
-			return newGraph;
-		};
-
-		const addNode = (node) => {
-			const id = nodeSeq.getNext();
-
-			this.nodes[id] = Object.assign({}, node);
-
-			this.nodes[id].id = id;
-			this.nodes[id].graph = this;
-
-			if (options.onAddNode) {
-				(options.onAddNode)(this.nodes[id], options.context);
-			}
-
-			this.nodeCount++;
-			return this.nodes[id];
-		};
-
-		const addEdge = (edge) => {
-			const id = edgeSeq.getNext();
-			this.edges[id] = Object.assign({}, edge);
-
-			this.edges[id].id = id;
-			this.edges[id].graph = this;
-
-			this.nodes[edge.from].outbound[edge.to] = edge;
-			this.nodes[edge.to].inbound[edge.from] = edge;
-			this.nodes[edge.from].numOutbound++;
-			this.nodes[edge.to].numInbound++;
-
-			if (options.onAddEdge) {
-				(options.onAddEdge)(this.edges[id], options.context);
-			}
-
-			this.edgeCount++;
-			return this.edges[id];
-		};
-
-		const removeNode = (node) => {
-			const id = getId(node);
-
-			if (options.onRemoveNode) {
-				(options.onRemoveNode)(this.nodes[id], options.context);
-			}
-
-			for (let e in this.edges) {
-				if ((this.edges[e]) && ((this.edges[e].from.id == id) || (this.edges[e].to.id == id))) {
-					this.removeEdge(e);
-				}
-			}
-
-			this.nodeCount--;
-			delete this.nodes[id];
-		};
-
-		const removeEdge = (edge) => {
-			const id = getId(edge);
-
-			if (!this.edges[id]) return;
-
-			if (options.onRemoveEdge) {
-				(options.onRemoveEdge)(this.edges[id], options.context);
-			}
-
-			const fromRode = this.nodes[this.edges[id].from.id];
-			const toNode = this.nodes[this.edges[id].to.id];
-			delete fromNode.outbound[this.edges[id].to.id];
-			delete toNode.inbound[this.edges[id].from.id];
-			fromNode.numOutbound--;
-			toNode.numInbound--;
-
-			this.edgeCount--;
-			delete this.edges[id];
-		};
-
-		const hasEdge = (from, to) => from.outbound[getId(to)] !== undefined;
-		const getNodeById = (nodeId) => this.nodes[nodeId];
-		const getEdgeById = (edgeId) => this.edges[edgeId];
-		const inflateEdges = (edges) => {};
-		const inflateNodes = (nodes) => {};
-		const getNodes = () => this.nodes;
-		const getEdges = () => this.edges;
-		const link = (from, to, payload) => this.addEdge(edge(
-			getId(from),
-			getId(to),
-			payload
-		));
-
-		return {
-			nodes,
-			edges,
-			nodeCount,
-			edgeCount,
-			pack,
-			mergeWith,
-			addNode,
-			addEdge,
-			hasEdge,
-			removeNode,
-			removeEdge,
-			getNodeById,
-			getEdgeById,
-			inflateNodes,
-			inflateEdges,
-			getNodes,
-			getEdges,
-			link
-		};
+		return newGraph;
 	};
 
-	const empty = () => fragment({
-		nodeCount: 0,
-		edgeCount: 0,
-		nodes: {},
-		edges: {}
-	});
+	const mergeWith = (fragment) => {
+		const operand = Graph.fragment(fragment);
+		const newGraph = pack();
+		const mapping = {};
 
-	const node = (payload, metadata) => ({
-		type: 'node',
-		payload,
-		metadata,
-		numOutbound: 0,
-		numInbound: 0,
-		outbound: {},
-		inbound: {},
-		inGraph: () => this.graph !== undefined,
-		getOutboundEdges: () => this.outbound,
-		getInboundEdges: () => this.inbound
-	});
+		for (let n in operand.nodes) {
+			if (operand.nodes[n]) {
+				mapping[n] = newGraph.addNode(Object.assign({}, operand.nodes[n]));
+			}
+		}
 
-	const edge = (from, to, payload, metadata) => ({
-		type: 'edge',
-		payload,
-		metadata,
-		from: getId(from),
-		to: getId(to),
-		inGraph: () => this.graph !== undefined
-	});
+		for (let e in operand.edges) {
+			if (operand.edges[e]) {
+				let id = newGraph.addEdge(Object.assign({}, operand.edges[e]));
+				newGraph.edges[id].from = mapping[newGraph.edges[id].from];
+				newGraph.edges[id].to = mapping[newGraph.edges[id].to];
+			}
+		}
+
+		return newGraph;
+	};
+
+	const addNode = (node) => {
+		const id = nodeSeq.getNext();
+
+		nodes[id] = Object.assign({}, node);
+
+		nodes[id].id = id;
+		nodes[id].graph = this;
+
+		if (options.onAddNode) {
+			(options.onAddNode)(nodes[id], options.context);
+		}
+
+		nodeCount++;
+		return nodes[id];
+	};
+
+	const addEdge = (edge) => {
+		const id = edgeSeq.getNext();
+		edges[id] = Object.assign({}, edge);
+
+		edges[id].id = id;
+		edges[id].graph = this;
+
+		nodes[edge.from].outbound[edge.to] = edge;
+		nodes[edge.to].inbound[edge.from] = edge;
+		nodes[edge.from].numOutbound++;
+		nodes[edge.to].numInbound++;
+
+		if (options.onAddEdge) {
+			(options.onAddEdge)(edges[id], options.context);
+		}
+
+		edgeCount++;
+		return edges[id];
+	};
+
+	const removeNode = (node) => {
+		const id = getId(node);
+
+		if (options.onRemoveNode) {
+			(options.onRemoveNode)(nodes[id], options.context);
+		}
+
+		for (let e in edges) {
+			if ((edges[e]) && ((edges[e].from.id == id) || (edges[e].to.id == id))) {
+				removeEdge(e);
+			}
+		}
+
+		nodeCount--;
+		delete nodes[id];
+	};
+
+	const removeEdge = (edge) => {
+		const id = getId(edge);
+
+		if (!edges[id]) return;
+
+		if (options.onRemoveEdge) {
+			(options.onRemoveEdge)(edges[id], options.context);
+		}
+
+		const fromRode = nodes[edges[id].from.id];
+		const toNode = nodes[edges[id].to.id];
+		delete fromNode.outbound[edges[id].to.id];
+		delete toNode.inbound[edges[id].from.id];
+		fromNode.numOutbound--;
+		toNode.numInbound--;
+
+		edgeCount--;
+		delete edges[id];
+	};
+
+	const hasEdge = (from, to) => !!from.outbound[getId(to)];
+	const getNodeById = (nodeId) => nodes[nodeId];
+	const getEdgeById = (edgeId) => edges[edgeId];
+	const inflateEdges = (edges) => {};
+	const inflateNodes = (nodes) => {};
+	const getNodes = () => nodes;
+	const getEdges = () => edges;
+	const link = (from, to, payload) => addEdge(edge(
+		getId(from),
+		getId(to),
+		payload
+	));
 
 	return {
-		getId,
-		fragment,
-		empty,
-		node,
-		edge
+		nodes,
+		edges,
+		nodeCount,
+		edgeCount,
+		pack,
+		mergeWith,
+		addNode,
+		addEdge,
+		hasEdge,
+		removeNode,
+		removeEdge,
+		getNodeById,
+		getEdgeById,
+		inflateNodes,
+		inflateEdges,
+		getNodes,
+		getEdges,
+		link
 	};
+};
+
+const empty = () => fragment({
+	nodeCount: 0,
+	edgeCount: 0,
+	nodes: {},
+	edges: {}
+});
+
+const node = (payload, metadata) => ({
+	type: 'node',
+	payload,
+	metadata,
+	numOutbound: 0,
+	numInbound: 0,
+	outbound: {},
+	inbound: {},
+	inGraph: () => this.graph !== undefined,
+	getOutboundEdges: () => this.outbound,
+	getInboundEdges: () => this.inbound
+});
+
+const edge = (from, to, payload, metadata) => ({
+	type: 'edge',
+	payload,
+	metadata,
+	from: getId(from),
+	to: getId(to),
+	inGraph: () => this.graph !== undefined
+});
+
+export default {
+	getId,
+	fragment,
+	empty,
+	node,
+	edge
 };
---
+++ test/graph.js
@@ -0,0 +1,47 @@
+import { assert } from 'chai';
+
+import graphFactory from '../dist/graph';
+
+describe('Graph', function() {
+	it('should retrieve the id property of an object', function() {
+		const id = 3;
+		const obj = { id };
+		assert.equal(id, graphFactory.getId(obj));
+	});
+
+	it('should create a standalone node with payload and metadata', function() {
+		const node = graphFactory.node({ x: 1 }, { y: 2 });
+		assert.equal(1, node.payload.x);
+		assert.equal(2, node.metadata.y);
+	});
+
+	it('should create a standalone edge with payload and metadata', function() {
+		const node1 = graphFactory.node();
+		const node2 = graphFactory.node();
+		const edge = graphFactory.edge(node1, node2, { x: 1 }, { y: 2 })
+		assert.equal(1, edge.payload.x);
+		assert.equal(2, edge.metadata.y);
+	});
+
+	it('should create an empty graph', function() {
+		const graph = graphFactory.empty();
+		assert.isObject(graph.nodes);
+		assert.isObject(graph.edges);
+	});
+
+	it('should add a node to a graph', function() {
+		const graph = graphFactory.empty();
+		const node = graph.addNode(graphFactory.node());
+		const retrieved = graph.getNodeById(node.id);
+		assert.equal(node.id, retrieved.id);
+	});
+
+	it('should add an edge to a graph', function() {
+		const graph = graphFactory.empty();
+		const node1 = graph.addNode(graphFactory.node());
+		const node2 = graph.addNode(graphFactory.node());
+		const edge = graph.addEdge(graphFactory.edge());
+		const retrieved = graph.getEdgeById(edge.id);
+		assert.equal(edge.id, retrieved.id);
+	});
+});
