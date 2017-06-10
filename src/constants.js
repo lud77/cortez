f7ec6926850e11c14c59a0bbad71bbc9edd71483
lud77 <stefano.butera@nethnic.com>
2017-06-10 18:02:02 +0100

    removed constants file

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-export default {
-    DIR_OUTBOUND: 0,
-    DIR_INBOUND: 1,
-    DIR_ANY: 2
-};
--- src/cortez.js
+++ src/cortez.js
@@ -1,6 +1,5 @@
 import get from "lodash/get";
 
-import constants from "./constants";
 import graph from "./graph";
 import node from "./node";
 import edge from "./edge";
